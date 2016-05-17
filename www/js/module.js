158bbe05632e678ca2c334f93f2eb3dcbf6b7ac9
hllane08 <harvin.llanes@gmail.com>
2016-05-16 21:55:57 -0600

    Add ngCordova reference folder

--- www/js/module.js
+++ www/js/module.js
@@ -1,9 +1,9 @@
-(function(){
+(function () {
     'use strict';
-     angular.module('app',[
-          'ionic',
+
+     angular.module('app', [
+      'ionic',
 	  'ngCordova',
-	  'ngCookies',
 	  'restangular'
     ]);
 })();
