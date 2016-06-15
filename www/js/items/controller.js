3c68badcb8ed47f2ce87efb2c3f5a3421da63daa
Harvin Llanes <harvinllanes@moi-solutions.com>
2016-06-15 00:12:09 -0600

    Show left items for menu get id from api

--- www/js/items/controller.js
+++ www/js/items/controller.js
@@ -8,15 +8,25 @@
 	    $scope,
 	    $state,
 	    $ionicLoading,
-	    authService)
+	    authService,
+	    Restangular)
     {
-        console.log('loading Controller');
         var vm = this;
 	vm.logout = logout;
-       
+	vm.assignItems = assignItems;
+	vm.items = [];
+        vm.onIonicViewLoaded = onIonicViewLoaded; 
+	$scope.$on('$ionicView.loaded',vm.onIonicViewLoaded)
         function logout(){
 	    return authService.logout();
 	}
-
+	function onIonicViewLoaded(){
+	   Restangular.all('items')
+	       .getList()
+	       .then(assignItems);
+	}
+	function assignItems(response){
+	   vm.items = response.plain();
+	}
     }
  })();
